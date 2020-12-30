4748d14f8e70763a378e1c104958f94ef0bcbf97
Menamot <kimimoyan@outlook.com>
2020-12-30 18:50:52 +0800

    4

---
+++ 12/second.py
@@ -0,0 +1,9 @@
+# -*- coding =utf-8 -*-
+# @Time : 2020/12/30 15:47
+# @Author : 陈文龙
+# @File : second.py
+# @Software : PyCharm
+import requests
+
+url = 'https://www.baidu.com'
+response = requests.get(url)
